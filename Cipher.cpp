844a731c8784820580a46933167b3e0d1b541d59
moise7000 <77933570+moise7000@users.noreply.github.com>
2025-10-18 22:30:42 +0200

    [+] missing fileé

---
+++ Cipher.cpp
@@ -0,0 +1,12 @@
+//
+// Created by ewan decima on 10/18/25.
+//
+
+#include "Cipher.h"
+
+
+const std::string Cipher::BASE64_CHARS =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+const std::string Cipher::BASE32_CHARS =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";

1cfd30a12320ac8b64f650ca485a03b84d9a5228
moise7000 <77933570+moise7000@users.noreply.github.com>
2025-10-29 12:31:45 +0100

    [+] Clean architecture files

--- ComputerName.cpp
+++ ComputerName.cpp
@@ -2,20 +2,22 @@
 // Created by ewan decima on 10/29/25.
 //
 
-#include <iostream>
-#include <string>
 #include <Windows.h>
+#include "ComputerName.h"
 
 /** |------------------------------------------------------|
 *   |        Fonction qui renvoie a priori "LHS-PC"        |
 */  |------------------------------------------------------|
 
-std::string GetComputerName() {
+
+std::string getName() {
     char buffer[MAX_COMPUTERNAME_LENGTH + 1];
     DWORD size = sizeof(buffer);
-    if (!GetComputerNameA(buffer, &size)) {
+
+    if (GetComputerNameA(buffer, &size)) {
         return std::string(buffer);
-    } else {
-        return "";
     }
+
+    return "";
 }
+
---
+++ ComputerName.h
@@ -0,0 +1,13 @@
+//
+// Created by ewan decima on 10/29/25.
+//
+
+#ifndef MAL_WHERE_COMPUTERNAME_H
+#define MAL_WHERE_COMPUTERNAME_H
+
+#include <string>
+
+std::string getName();
+
+
+#endif //MAL_WHERE_COMPUTERNAME_H

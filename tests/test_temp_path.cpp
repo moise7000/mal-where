79e431c0dfd25beff27547f78081fe6c1494f1b3
moise7000 <77933570+moise7000@users.noreply.github.com>
2025-11-05 19:48:12 +0100

    [+] get temp from environement variable

---
+++ tests/test_temp_path.cpp
@@ -0,0 +1,14 @@
+//
+// Created by ewan decima on 11/5/25.
+//
+
+#include "TestingTools.h"
+#include "../env/SystemEnvironment.h"
+
+int main() {
+    const std::string tmpPath = systemEnvironment::getTempPath();
+
+    const bool passed = tmpPath ==  "C:\\Users\\lhs\\AppData\\Local\\Temp";
+
+    if (passed) TestingTools::printGreen("Test passed: tmp path"); else TestingTools::printRed("Test failed");
+}

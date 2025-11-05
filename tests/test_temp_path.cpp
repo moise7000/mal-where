abb423e72ecebf39c80e8ff9ecff32b226e1451a
moise7000 <77933570+moise7000@users.noreply.github.com>
2025-11-05 20:25:34 +0100

    [~] fix

--- tests/test_temp_path.cpp
+++ tests/test_temp_path.cpp
@@ -2,13 +2,16 @@
 // Created by ewan decima on 11/5/25.
 //
 
+#include <iostream>
+#include <ostream>
+
 #include "TestingTools.h"
 #include "../env/SystemEnvironment.h"
 
 int main() {
     const std::string tmpPath = systemEnvironment::getTempPath();
-
-    const bool passed = tmpPath ==  "C:\\Users\\lhs\\AppData\\Local\\Temp";
+    
+    const bool passed = tmpPath ==  "C:\\Users\\lhs\\AppData\\Local\\Temp\\";
 
     if (passed) TestingTools::printGreen("Test passed: tmp path"); else TestingTools::printRed("Test failed");
 }

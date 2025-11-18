e4e7ffe51b3f02f86c62ec1b98d62a3dec738c63
moise7000 <77933570+moise7000@users.noreply.github.com>
2025-11-18 19:22:47 +0100

    [~] reformating test

--- tests/test_username.cpp
+++ tests/test_username.cpp
@@ -6,26 +6,18 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+
+#include "TestingTools.h"
 #include "../obfuscation_methods/get_username.h"
 
 int main() {
-    std::cout << "  Test getUsername() - Windows 7+    " << std::endl;
-    std::cout << std::endl;
 
-    // Get username
-    std::string username = getUsername();
-
+    const std::string username = getUsername();
     if (!username.empty()) {
-        std::cout << "USERNAME = \"" << username << "\"" << std::endl;
-        std::cout << "[OK] Username retrieved" << std::endl;
+        const bool pass = (username == "lhs");
+        if (pass) TestingTools::printGreen("Test passed: username is lhs"); else TestingTools::printRed("Test failed: username is not lhs");
     } else {
-        std::cout << "USERNAME = (empty)" << std::endl;
-        std::cout << "[FAIL] Could not retrieve username" << std::endl;
+        TestingTools::printRed("Test failed: username is empty");
     }
-
-    std::cout << std::endl;
-    std::cout << "            Test Complete             " << std::endl;
-
-
     return 0;
 }
